27bf6735b078954e3fbb047d97b821dd1b089ce6
Adam Jensen <acjensen@gmail.com>
2016-08-26 17:42:06 -0400

    Initial commit with functional macOS framework for Tesseract

---
+++ TesseractFramework/TesseractFramework.h
@@ -0,0 +1,17 @@
+//
+//  TesseractFramework.h
+//  TesseractFramework
+//
+//  Created by Adam Jensen on 8/26/16.
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for TesseractFramework.
+FOUNDATION_EXPORT double TesseractFrameworkVersionNumber;
+
+//! Project version string for TesseractFramework.
+FOUNDATION_EXPORT const unsigned char TesseractFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TesseractFramework/PublicHeader.h>
+
+
